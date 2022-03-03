4dd845d964c714fb941330266587e129bfb82fb9
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-03 21:48:04 +0100

    :construction: Update .env & some migrations

--- database/migrations/2022_03_03_200324_create_addresses_table.php
+++ database/migrations/2022_03_03_200324_create_addresses_table.php
@@ -17,23 +17,21 @@ return new class extends Migration
             $table->uuid('uuid')->primary();
             $table->string('place_name', 255);
             $table->boolean('place_status')->default(1);
-            $table->text('address_number', 255);
-            $table->text('address_street', 255);
-            $table->text('address_postcode', 255);
-            $table->text('address_city', 255);
-            $table->point('address_lat');
-            $table->point('address_lon');
+            $table->string('address_number', 255);
+            $table->string('address_street', 255);
+            $table->string('address_postcode', 255);
+            $table->string('address_city', 255);
+            $table->string('address_country', 3);
+            $table->float('address_lat');
+            $table->float('address_lon');
             $table->string('details_openinghours', 255)->nullable();
-            $table->integer('details_phone', 255)->nullable();
+            $table->integer('details_phone')->nullable();
             $table->string('details_website', 255)->nullable();
             $table->string('details_wikidata', 255)->nullable();
             $table->text('description')->nullable();
             $table->uuid('category_uuid');
             $table->foreign('category_uuid')
                 ->references('uuid')->on('categories__')->onDelete('cascade');
-            $table->uuid('country_uuid');
-            $table->foreign('country_uuid')
-                ->references('uuid')->on('countries__')->onDelete('cascade');
             $table->integer('osm_id')->nullable();
             $table->integer('osm_place_id')->nullable();
             $table->string('gmap_pluscode', 255)->nullable();
