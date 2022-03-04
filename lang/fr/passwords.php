dbc2108b2c8d1651d624ae9bded0ef39e37302fc
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-04 16:50:29 +0100

    Create passwords.php

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a bien été réinitialisé !',
+    'sent' => 'NOus vous avez fait parvenir un lien de réinitialisation de mot de passe !',
+    'throttled' => 'Veuillez patienter avec de retenter.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => 'Nous ne parvenons pas à trouver d\'utilisateur avec ce courriel.',
+
+];
