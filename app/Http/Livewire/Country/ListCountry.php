41aca6e4b7ea90d33cc448c4f26aa07aabfc4c6d
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-19 19:29:50 +0100

    Add country support

--- app/Http/Livewire/Country/ListCountry.php
+++ app/Http/Livewire/Country/ListCountry.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Livewire\Country;
 
+use Papposilene\Geodata\Models\Country;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ListCountry extends Component
 {
+    use WithPagination;
+
+    public $filter = '';
+    public $page = 1;
+    public $search = '';
+    public Country $country;
+
+    protected $queryString = [
+        'filter' => ['except' => ''],
+        'page' => ['except' => 1],
+        'search' => ['except' => ''],
+    ];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.country.list-country');
+        $countries = Country::where('cca3', 'like', '%'.$this->search.'%')
+            ->orderBy('cca3', 'asc')
+            ->paginate(25);
+
+        return view('livewire.country.list-country', [
+            'countries' => $countries,
+        ]);
     }
 }
