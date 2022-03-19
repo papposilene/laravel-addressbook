cc86600ecb98c99af55b66d2246e7e5158db1fff
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-19 19:29:29 +0100

    Add lang support

---
+++ lang/fr/app.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Country Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'address' => 'adresse',
+    'addresses' => 'adresses',
+    'category' => 'catégorie',
+    'categories' => 'catégories',
+    'country' => 'pays',
+    'countries' => 'pays',
+
+];
