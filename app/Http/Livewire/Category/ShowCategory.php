494c23ab429c950cdc093f97880f74ca007f8832
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-21 13:08:06 +0100

    Update ShowCategory.php

--- app/Http/Livewire/Category/ShowCategory.php
+++ app/Http/Livewire/Category/ShowCategory.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Livewire\Category;
 
+use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowCategory extends Component
 {
+    use WithPagination;
+
+    public Category $category;
+    public $filter = '';
+    public $page = 1;
+    public $search = '';
+    public $slug;
+
+    protected $queryString = [
+        'filter' => ['except' => ''],
+        'page' => ['except' => 1],
+        'search' => ['except' => ''],
+    ];
+
+    public function mount($slug)
+    {
+        $this->subcategory = Subcategory::where('slug', $this->slug);
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.category.show-category');
+        return view('livewire.category.show-category', [
+            'subcategory' => $subcategory,
+        ]);
     }
 }
