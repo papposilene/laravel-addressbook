0e09a4abcf632ef161b51ef71bcd3c97f1eab0c4
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-21 12:54:05 +0100

    Update app.php

--- lang/fr/app.php
+++ lang/fr/app.php
@@ -9,15 +9,32 @@ return [
     */
 
     // Plain strings
+
+    'about' => 'a propos',
     'actions' => 'actions',
+    'all' => 'tout',
     'count' => 'total',
+    'create' => 'ajouter',
+    'delete' => 'supprimer',
     'description' => 'description',
+    'ddmmyyyy' => 'JJ/MM/AAAA',
+    'export' => 'exporter',
+    'https' => 'https://www.',
+    'import' => 'importer',
+    'informations' => 'informations',
     'latitude' => 'latitude',
+    'loading' => 'chargement des données en cours...',
     'longitude' => 'longitude',
+    'map' => 'carte',
     'name' => 'nom',
     'no' => 'non',
     'nothing' => 'aucun résultat',
+    'other' => 'autre',
+    'private' => 'privé',
+    'public' => 'public',
+    'save' => 'enregistrer',
     'search' => 'rechercher',
+    'slug' => 'identifiant unique textuel',
     'title' => 'intitulé',
     'total' => 'total',
     'translation' => 'traduction',
@@ -26,9 +43,10 @@ return [
     'yes' => 'oui',
 
     // Placeholders strings
+    'delete_' => 'supprimer :pronoun :what',
+    'edit_' => 'modifier :pronoun :what',
     'list_of' => 'liste :pronoun :what',
     'name_of' => 'nom :pronoun :what',
+    'save_' => 'ajouter :pronoun :what',
     'title_of' => 'titre :pronoun :what',
-
-
 ];
