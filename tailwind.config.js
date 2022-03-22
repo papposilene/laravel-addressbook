227b1ea741852689533e8676eccca1fc9c374ad3
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-22 11:58:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         './resources/views/**/*.blade.php',
     ],
 
-    darkMode: 'media',
+    darkMode: 'class',
 
     theme: {
         colors: {
@@ -17,11 +17,28 @@ module.exports = {
             current: 'currentColor',
             black: colors.black,
             white: colors.white,
-            gray: colors.slate,
-            green: colors.emerald,
-            purple: colors.violet,
-            yellow: colors.amber,
-            pink: colors.fuchsia,
+            slate: colors.slate,
+            gray: colors.gray,
+            zinc: colors.zinc,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
         },
         extend: {
             fontFamily: {
