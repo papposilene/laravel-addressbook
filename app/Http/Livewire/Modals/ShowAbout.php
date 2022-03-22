c93862433cc861dc5e6c59e4125384d0c468e5a2
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-22 11:58:53 +0100

    Update Livewire

---
+++ app/Http/Livewire/Modals/ShowAbout.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modals;
+
+use Livewire\Component;
+
+class ShowAbout extends Component
+{
+    public function render()
+    {
+        return view('livewire.modals.show-about');
+    }
+}
