dbf0ad0ddca99547252e78e90c4986fee7354618
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-22 07:05:58 +0100

    Add FontAwesome support

--- resources/views/livewire/country/list-country.blade.php
+++ resources/views/livewire/country/list-country.blade.php
@@ -9,11 +9,11 @@
 
     <div>
         <!-- @see https://fontawesome.com/docs/web/add-icons/svg-symbols -->
-        <i data-fa-symbol="create" class="fas fa-plus fa-fw"></i>
-        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
-        <i data-fa-symbol="edit" class="fas fa-pencil fa-fw"></i>
-        <i data-fa-symbol="favorite" class="fas fa-star fa-fw"></i>
-        <i data-fa-symbol="show" class="fas fa-ellipsis fa-fw"></i>
+        <i data-fa-symbol="create" class="fas fa-plus fa-fw h-8 w-8"></i>
+        <i data-fa-symbol="delete" class="fas fa-trash fa-fw h-8 w-8"></i>
+        <i data-fa-symbol="edit" class="fas fa-pencil fa-fw h-8 w-8"></i>
+        <i data-fa-symbol="favorite" class="fas fa-star fa-fw h-8 w-8"></i>
+        <i data-fa-symbol="show" class="fas fa-ellipsis fa-fw h-8 w-8"></i>
 
         <div class="max-w-7xl mx-auto py-5 px-6">
             @if ($errors->any())
