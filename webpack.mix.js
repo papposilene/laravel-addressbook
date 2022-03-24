2ea9d029addc4e68485cfe6ec706b900bcef4699
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-24 11:51:58 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.options({
+    processCssUrls: false,
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
