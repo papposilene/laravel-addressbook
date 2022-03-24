692952d521d2e5977b4ee043d091ca915c388efd
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-24 22:58:45 +0100

    Livewire: update controlers

--- app/Http/Livewire/Interfaces/Wikipedia.php
+++ app/Http/Livewire/Interfaces/Wikipedia.php
@@ -18,25 +18,25 @@ class Wikipedia extends Component
 
     public function render()
     {
+        $lang = app()->getLocale();
         $slug = $this->wikidata;
-        $cache = Wikipedia::where('wikidata_id', $slug)->first();
+        $cache = Wikidata::where('wikidata_id', $slug)->first();
 
         if(is_null($cache)) {
-            $lang = app()->getLocale();
             $dataJson = file_get_contents('https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&props=sitelinks&ids=' . $slug);
             $dataFile = json_decode($dataJson, true);
-            if(property_exists($dataFile['entities'][$slug]['sitelinks'], $lang . 'wiki')) {
-                $title = $dataFile['entities'][$slug]['sitelinks'][$lang . 'wiki'];
+            if($dataFile['entities'][$slug]['sitelinks'][$lang . 'wiki']) {
+                $title = $dataFile['entities'][$slug]['sitelinks'][$lang . 'wiki']['title'];
             } else {
                 $lang = 'en';
-                $title = $dataFile['entities'][$slug]['sitelinks']['enwiki'];
+                $title = $dataFile['entities'][$slug]['sitelinks']['enwiki']['title'];
             }
 
             $pediaJson = file_get_contents('https://' . $lang . '.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=' . urlencode($title));
             $pediaFile = json_decode($pediaJson, true);
-            $pediaData = $pediaFile['entities'][$this->wikidata]['sitelinks'][$lang . 'wiki'];
+            //$pediaData = $pediaFile['entities'][$this->wikidata]['sitelinks'][$lang . 'wiki'];
 
-            dd($pediaData);
+            dd(in_array('extracts', $pediaFile));
 
             $cache = Wikidata::create([
                 'address_uuid' => $this->address_uuid,
