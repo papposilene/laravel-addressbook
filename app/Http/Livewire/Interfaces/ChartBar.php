1367be658f23bd5eaae69ed37189c2998c473841
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-27 23:56:11 +0200

    Livewire: add bar chart support

---
+++ app/Http/Livewire/Interfaces/ChartBar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Interfaces;
+
+use Livewire\Component;
+
+class ChartBar extends Component
+{
+    public function render()
+    {
+        return view('livewire.interfaces.chart-bar');
+    }
+}
