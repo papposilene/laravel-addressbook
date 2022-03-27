5dead95247e6cb23f5a2da3554833775dc70d367
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-27 23:56:31 +0200

    Livewire: add doughnut chart support

---
+++ app/Http/Livewire/Interfaces/ChartDoughnut.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Interfaces;
+
+use Livewire\Component;
+
+class ChartDoughnut extends Component
+{
+    public function render()
+    {
+        return view('livewire.interfaces.chart-doughnut');
+    }
+}
