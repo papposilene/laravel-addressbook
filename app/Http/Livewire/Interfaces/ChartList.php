f28f1498ab94fb26a6975016cd527034534b3700
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-27 23:57:49 +0200

    Livewire: add list chart support

---
+++ app/Http/Livewire/Interfaces/ChartList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Interfaces;
+
+use Livewire\Component;
+
+class ChartList extends Component
+{
+    public function render()
+    {
+        return view('livewire.interfaces.chart-list');
+    }
+}
