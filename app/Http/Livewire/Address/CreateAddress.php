40fa3a537679e1e7bcb358cee9429466d8483b5f
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-28 21:43:57 +0200

    Livewire: add create and edit address support

---
+++ app/Http/Livewire/Address/CreateAddress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Address;
+
+use Livewire\Component;
+
+class CreateAddress extends Component
+{
+    public function render()
+    {
+        return view('livewire.address.create-address');
+    }
+}
---
+++ app/Http/Livewire/Address/EditAddress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Address;
+
+use Livewire\Component;
+
+class EditAddress extends Component
+{
+    public function render()
+    {
+        return view('livewire.address.edit-address');
+    }
+}
