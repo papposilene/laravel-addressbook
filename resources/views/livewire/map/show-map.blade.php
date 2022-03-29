1f13d65c4d1d7921d6f2cc4eb9f9bec51e1d4897
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-29 09:14:13 +0200

    Resources: update views

--- resources/views/livewire/map/show-map.blade.php
+++ resources/views/livewire/map/show-map.blade.php
@@ -10,8 +10,32 @@
         <i data-fa-symbol="favorite" class="fas fa-star fa-fw text-yellow-500"></i>
         <i data-fa-symbol="show" class="fas fa-ellipsis fa-fw text-green-500"></i>
 
-        <div class="flex flex-col w-full lg:max-w-7xl lg:mx-auto py-5 px-6">
-            <div id="leaflet-map" class="bg-red-200"></div>
+        <div class="flex flex-col w-full">
+            <div id="leaflet-addresses-map" class="bg-red-200 w-full h-screen" wire:ignore></div>
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener('livewire:load', function () {
+
+        const leafletMap = L.map('leaflet-addresses-map', {
+            center: [0, 0],
+            zoom: 4,
+            zoomControl: true
+        });
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(leafletMap);
+
+        L.control.locate({
+            'enableHighAccuracy': true,
+            'flyTo': true,
+            strings: {
+                title: "Show me where I am, yo!"
+            }
+        }).addTo(leafletMap);
+        L.marker([0, 0]).addTo(leafletMap);
+    })
+</script>
