3c7d5e3f0d58b44954895cdb96c36feee57710dc
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-29 12:04:17 +0200

    Livewire: update dashboard

--- resources/views/livewire/dashboard/show-dashboard.blade.php
+++ resources/views/livewire/dashboard/show-dashboard.blade.php
@@ -40,7 +40,7 @@
 
             <!-- Dashboard -->
             <div class="flex flex-row flex-wrap py-5">
-                <div class="flex flex-col w-full lg:w-1/3">
+                <div class="flex flex-col w-full lg:pr-1 lg:w-1/3">
                     <h3 class="bg-gray-600 text-gray-200 font-semibold text-xl p-3 rounded-t">
                         Pays
                     </h3>
@@ -73,6 +73,26 @@
                             </span>
                         </li>
                     </ul>
+                </div>
+
+                <div class="flex flex-col w-full lg:px-1 lg:w-1/3">
+                    <h3 class="bg-gray-600 text-gray-200 font-semibold text-xl p-3 rounded-t">
+                        Pays
+                    </h3>
+                    <div class="bg-gray-500 text-gray-200 p-3 mb-3 rounded-b">
+
+                    </div>
+                </div>
+
+                <div class="flex flex-col w-full lg:pl-1 lg:w-1/3">
+                    <h3 class="bg-gray-600 text-gray-200 font-semibold text-xl p-3 rounded-t">
+                        Pays
+                    </h3>
+                    <div class="bg-gray-500 text-gray-200 p-3 mb-3 rounded-b">
+                        @livewire('interfaces.chart-doughnut', [
+                            'api' => route('api.country.index', ['limit' => 10, 'orderby' => 'desc']),
+                        ])
+                    </div>
                 </div>
 
             </div>
