24d060aa8da381bdd83a7a5f129a2d6c492811a2
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-29 08:02:23 +0200

    Resources: update views

--- resources/views/livewire/address/create-address.blade.php
+++ resources/views/livewire/address/create-address.blade.php
@@ -1,3 +1,33 @@
+@section('title', @ucfirst(__('app.create_', ['pronoun' => __('address.pronoun_sg'), 'what' => __('address.address')])))
+
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-300 leading-tight">
+            <span class="inline-flex align-middle">
+                @ucfirst(__('app.list_of', ['pronoun' => __('address.pronoun_pl'), 'what' => __('address.addresses')]))
+            </span>
+        </h2>
+    </x-slot>
+
+    <div>
+        <!-- @see https://fontawesome.com/docs/web/add-icons/svg-symbols -->
+        <i data-fa-symbol="icons" class="fas fa-icons fa-fw"></i>
+        <i data-fa-symbol="create" class="fas fa-plus fa-fw text-green-400"></i>
+        <i data-fa-symbol="delete" class="fas fa-trash fa-fw text-red-400"></i>
+        <i data-fa-symbol="edit" class="fas fa-pencil fa-fw text-blue-400"></i>
+        <i data-fa-symbol="favorite" class="fas fa-star fa-fw text-yellow-400"></i>
+        <i data-fa-symbol="show" class="fas fa-ellipsis fa-fw text-green-400"></i>
+
+        <div class="max-w-7xl mx-auto py-5 px-6">
+            @if ($errors->any())
+                <div class="bg-red-400 border border-red-600 mb-5 p-3 text-white font-bold rounded shadow">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
