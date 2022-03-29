9d7cb43190ce5ca41b4f2d38dd0b5a44ba443603
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-29 09:13:20 +0200

    Livewire: add landing page

---
+++ app/Http/Livewire/Dashboard/ShowLanding.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard;
+
+use App\Models\Country;
+use App\Models\Subcategory;
+use Livewire\Component;
+
+class ShowLanding extends Component
+{
+    protected $categories;
+    protected $countries;
+
+    public function render()
+    {
+        $this->categories = Subcategory::withCount('hasAddresses')
+            ->orderBy('has_addresses_count', 'desc')
+            ->get();
+        $this->countries = Country::withCount('hasAddresses')
+            ->orderBy('cca3', 'asc')
+            ->get();
+
+        return view('livewire.dashboard.show-landing', [
+            'categories' => $this->categories->where('has_addresses_count', '>=', 1),
+            'countries' => $this->countries->where('has_addresses_count', '>=', 1),
+        ]);
+    }
+}
