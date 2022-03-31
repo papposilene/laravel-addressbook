615d65a5b49593edf9c4442a13aab28eabe7e63e
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-31 11:13:31 +0200

    Update geodata config

--- config/geodata.php
+++ config/geodata.php
@@ -4,7 +4,7 @@ return [
 
     'options' => [
 
-        /*
+        /**
          * You can decide to activate or desactivate some geodata sets.
          * Continents, subcontinents, countries, cities and currencies
          * are mandatory datasets and can not be desactivate.
@@ -16,40 +16,43 @@ return [
 
     'models' => [
 
-        /*
+        /**
          * The model you want to use as a Continent model needs to implement the
-         * `Papposilene\Geodata\Contracts\Continent` contract.
+         * `Papposilene\Geodata\Models\Continent` model.
          */
-
         'continents' => Papposilene\Geodata\Models\Continent::class,
 
-        /*
+        /**
          * The model you want to use as a Subcontinent model needs to implement the
-         * `Papposilene\Geodata\Contracts\Subcontinent` contract.
+         * `Papposilene\Geodata\Models\Subcontinent` model.
          */
-
         'subcontinents' => Papposilene\Geodata\Models\Subcontinent::class,
 
-        /*
+        /**
          * The model you want to use as a Country model needs to implement the
-         * `Papposilene\Geodata\Contracts\Country` contract.
+         * `Papposilene\Geodata\Models\Country` model.
          */
-
         'countries' => Papposilene\Geodata\Models\Country::class,
 
-        /*
-         * The model you want to use as a City model needs to implement the
-         * `Papposilene\Geodata\Contracts\City` contract.
+        /**
+         * The model you want to use as a Region model needs to implement the
+         * `Papposilene\Geodata\Models\Region` model.
          */
 
+        'regions' => Papposilene\Geodata\Models\Region::class,
+
+        /**
+         * The model you want to use as a City model needs to implement the
+         * `Papposilene\Geodata\Models\City` model.
+         */
         'cities' => Papposilene\Geodata\Models\City::class,
 
-        /*
+        /**
          * The model you want to use as a Currency model needs to implement the
-         * `Papposilene\Geodata\Contracts\Currency` contract.
+         * `Papposilene\Geodata\Models\Currency` model.
          */
 
-        'currencies' => Papposilene\Geodata\Models\Currency::class,
+        //'currencies' => Papposilene\Geodata\Models\Currency::class,
 
     ],
 
