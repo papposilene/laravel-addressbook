6e3be5e8e454caafa20bad97c3f01ef1d869c6fc
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-04-14 21:29:58 +0200

    Controllers: update Livewire

--- app/Http/Livewire/Map/ShowMap.php
+++ app/Http/Livewire/Map/ShowMap.php
@@ -2,12 +2,48 @@
 
 namespace App\Http\Livewire\Map;
 
+use App\Models\Category;
+use App\Models\Country;
+use Illuminate\Http\Request;
 use Livewire\Component;
+use Papposilene\Geodata\Models\Continent;
 
 class ShowMap extends Component
 {
-    public function render()
+    public string $api;
+    public string $center = '[0, 0]';
+    public int $zoom = 4;
+
+    public function render(Request $request)
     {
-        return view('livewire.map.show-map');
+        $cca3 = $request->get('country', null);
+        $region = $request->get('region', null);
+        $category = $request->get('category', null);
+
+        if($cca3) {
+            $country = Country::where('cca3', $cca3)->firstOrFail();
+            $this->center = '['.$country->lon.', '.$country->lat.']';
+            $this->zoom = 6;
+        }
+
+        $this->categories = Category::withCount('hasAddresses')
+            ->orderBy('has_addresses_count', 'desc')
+            ->get();
+        $this->continents = Continent::orderBy('name', 'asc')
+            ->get();
+
+        $this->api = route('api.address.index', [
+            'country' => $cca3,
+            'region' => $region,
+            'category' => $category
+        ]);
+
+        return view('livewire.map.show-map', [
+            'api' => $this->api,
+            'center'=> $this->center,
+            'countries' => $this->continents,
+            'categories' => $this->categories,
+            'zoom'=> $this->zoom,
+        ])->layout('layouts.map');
     }
 }
