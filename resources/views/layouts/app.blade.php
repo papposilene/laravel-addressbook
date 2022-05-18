fc714c482b6533ac05af662d3be80949dcae1043
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-18 09:05:25 +0200

    Views: update app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
             <!-- Page Heading -->
             @if (isset($header))
             <header class="flex bg-gray-800 shadow">
-                <div class="flex max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 w-full">
                     {{ $header }}
                 </div>
             </header>
