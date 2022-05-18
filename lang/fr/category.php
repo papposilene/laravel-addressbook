308ce141a037dc8a1a025dcb1b12cff0a14dd37f
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-18 11:19:57 +0200

    Lang: update fr

--- lang/fr/category.php
+++ lang/fr/category.php
@@ -18,7 +18,11 @@ return [
 
     'all' => 'toutes les catégories',
     'name' => 'nom',
-
-
+    'translation' => 'traduction du nom',
+    'description' => 'description de la catégorie',
+    'iconImageLabel' => 'nom de l’icône',
+    'iconImageHelper' => 'Le nom de l’icône doit provenir de la bibliothèque [FontAwesome](https://fontawesome.com/search?m=free&s=solid%2Cbrands), sur le site de laquelle vous trouverez l’ensemble des icônes disponibles.',
+    'iconOptionsLabel' => 'options d’affichage',
+    'iconOptionsHelper' => 'Les attributs de customisation sont disponibles sur le site de [TailwindCSS](https://tailwindcss.com/docs/text-color).',
 
 ];
