e6faa0a75327fc80bc6c7d629b1f438c7cdf2d36
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-18 11:19:48 +0200

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,13 @@ module.exports = {
         },
     },
 
+    safelist: [
+        'bg-violet-900', 'bg-violet-600', 'bg-emerald-900', 'bg-emerald-600',
+        'bg-red-900', 'bg-red-400', 'bg-slate-900', 'bg-slate-600',
+        'bg-sky-800', 'bg-sky-200', 'bg-stone-700', 'bg-stone-300',
+        'bg-yellow-900', 'bg-yellow-600',
+    ],
+
     plugins: [
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/forms'),
