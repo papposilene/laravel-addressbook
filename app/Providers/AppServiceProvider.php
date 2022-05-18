71c86a7e07858d0eeed5be51f7847d28956509fa
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-18 11:20:42 +0200

    Providers: add gate for admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +26,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Permissions
+        Gate::define('manage_categories', function(User $user) {
+            return $user->is_admin == 1;
+        });
+        Gate::define('manage_addresses', function(User $user) {
+            return $user->is_admin == 1;
+        });
+
+        // Blade directives
         Blade::directive('camel', function ($expression) {
             return "<?php echo Illuminate\Support\Str::camel($expression); ?>";
         });
