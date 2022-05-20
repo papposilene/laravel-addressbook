db1ebd0adce0f6fe6c266313220612867d747a3e
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-20 12:21:06 +0200

    Database: update seeders

--- database/seeders/AddressesSeeder.php
+++ database/seeders/AddressesSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Address;
-use App\Models\City;
 use App\Models\Country;
-use App\Models\Region;
 use App\Models\Subcategory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,13 +12,6 @@ use Illuminate\Support\Str;
 
 class AddressesSeeder extends Seeder
 {
-    private function getEmail()
-    {
-        // Nominatim's Usage Policy
-        // @see https://operations.osmfoundation.org/policies/nominatim/
-        return env('MAIL_FROM_ADDRESS', new \Exception('No MAIL_FROM_ADDRESS key into your .env'));
-    }
-
     /**
      * Run the database seeds.
      *
@@ -46,7 +37,7 @@ class AddressesSeeder extends Seeder
                 $cityLevel = [];
                 $osmId = $data->geolocation->osm_id;
 
-                $dataJson = file_get_contents('https://nominatim.openstreetmap.org/details.php?addressdetails=1&format=json&email=' . $this->getEmail() . '&osmtype=' . substr($osmId, 0, 1) . '&osmid=' . substr($osmId, 1));
+                $dataJson = file_get_contents('https://nominatim.openstreetmap.org/details.php?addressdetails=1&format=json&email=' . getNominatimEmail() . '&osmtype=' . substr($osmId, 0, 1) . '&osmid=' . substr($osmId, 1));
                 $dataFile = json_decode($dataJson, true);
 
                 // Retrieve all administrative levels from Nominatim
@@ -69,11 +60,11 @@ class AddressesSeeder extends Seeder
                 }
 
                 if ($regionLevel) {
-                    $isRegion = $this->getRegion($regionLevel);
+                    $isRegion = getRegion($regionLevel);
                 }
 
                 if ($cityLevel) {
-                    $isCity = $this->getCity($cityLevel, $isRegion);
+                    $isCity = getCity($cityLevel, $isRegion);
                 }
 
                 $subcategory = Subcategory::where('slug', Str::slug($data->category->type, '-'))
@@ -102,90 +93,5 @@ class AddressesSeeder extends Seeder
                 ]);
             }
         }
-    }
-
-    private function getRegion(array $id)
-    {
-        $setRegion = Region::where('osm_id', $id['osm_type'] . $id['osm_id'])->first();
-        if (!is_null($setRegion)) {
-            return $setRegion;
-        }
-
-        $regionJson = file_get_contents('https://nominatim.openstreetmap.org/details.php?format=json&email=' . $this->getEmail() . '&osmtype=' . $id['osm_type'] . '&osmid=' . $id['osm_id']);
-        $regionData = json_decode($regionJson, true);
-
-        $setRegion = Region::where('name_slug', Str::slug($regionData['localname'], '-'))->first();
-        if (!is_null($setRegion)) {
-            return $setRegion;
-        }
-
-        $translations = [];
-        $getNames = $regionData['names'];
-        $getFiltered = array_filter($getNames, function ($key) {
-            return (str_starts_with($key, 'name:') && !str_ends_with($key, ':pronunciation'));
-        }, ARRAY_FILTER_USE_KEY);
-        foreach ($getFiltered as $key => $value) {
-            $lang = explode(':', $key);
-            $translations[$lang[1]] = $value;
-        }
-
-        $isCountry = Country::where('cca2', $regionData['country_code'])->firstOrFail();
-
-        return Region::create([
-            'country_cca2' => $isCountry->cca2,
-            'country_cca3' => $isCountry->cca3,
-            'region_cca2' => (array_key_exists('ISO3166-2', $regionData['extratags']) ? $regionData['extratags']['ISO3166-2'] : null),
-            'osm_id' => $id['osm_type'] . $id['osm_id'],
-            'osm_admin_level' => $regionData['admin_level'],
-            'osm_type' => $regionData['type'],
-            'name_slug' => Str::slug($regionData['localname'], '-'),
-            'name_local' => $regionData['localname'],
-            'name_translations' => $translations,
-            'extra' => [
-                'wikidata' => (array_key_exists('wikidata', $regionData['extratags']) ? $regionData['extratags']['wikidata'] : null),
-            ],
-        ]);
-    }
-
-    private function getCity(array $id, ?Region $isRegion)
-    {
-        $setCity = City::where('osm_id', $id)->first();
-        if (!is_null($setCity)) {
-            return $setCity;
-        }
-
-        $cityJson = file_get_contents('https://nominatim.openstreetmap.org/details.php?format=json&email=' . $this->getEmail() . '&osmtype=' . $id['osm_type'] . '&osmid=' . $id['osm_id']);
-        $cityData = json_decode($cityJson, true);
-
-        $setCity = City::where('name_slug', Str::slug($cityData['localname'], '-'))->first();
-        if (!is_null($setCity)) {
-            return $setCity;
-        }
-
-        $translations = [];
-        $getNames = $cityData['names'];
-        $getFiltered = array_filter($getNames, function ($key) {
-            return (str_starts_with($key, 'name:') && !str_ends_with($key, ':pronunciation'));
-        }, ARRAY_FILTER_USE_KEY);
-        foreach ($getFiltered as $key => $value) {
-            $lang = explode(':', $key);
-            $translations[$lang[1]] = $value;
-        }
-
-        $isCountry = Country::where('cca2', $cityData['country_code'])->firstOrFail();
-
-        return City::create([
-            'country_cca3' => $isCountry->cca3,
-            'region_uuid' => $isRegion->uuid ?? null,
-            'osm_id' => $id['osm_type'] . $id['osm_id'],
-            'osm_admin_level' => $cityData['admin_level'],
-            'osm_type' => $cityData['type'],
-            'name_slug' => Str::slug($cityData['localname'], '-'),
-            'name_local' => $cityData['localname'],
-            'name_translations' => $translations,
-            'extra' => [
-                'wikidata' => (array_key_exists('wikidata', $cityData['extratags']) ? $cityData['extratags']['wikidata'] : null),
-            ],
-        ]);
     }
 }
