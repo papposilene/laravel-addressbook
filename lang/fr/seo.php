8d6f2633551aab45096b6a98b11a05bb5d89f676
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-31 09:50:13 +0200

    Lang: update fr

---
+++ lang/fr/seo.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SEO Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    // Plain strings
+    'og_description' => 'Ce site répertorie toutes nos bonnes adresses glanées à travers le monde lors de voyages ou d’échanges.',
+    'twitter_site' => '@papposilene', // @username for the website used in the card footer.
+    'twitter_username' => '@papposilene', // @username for the content creator / author.
+];
